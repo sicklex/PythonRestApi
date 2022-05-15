6caceee6a937f8f9d4649a7907257184d90fbe5f
Claydson <71528039+sicklex@users.noreply.github.com>
2022-05-15 14:11:25 -0300

    resolvendo problema de autenticação

--- PixeonApi/settings.py
+++ PixeonApi/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'w13j2499',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
     }
